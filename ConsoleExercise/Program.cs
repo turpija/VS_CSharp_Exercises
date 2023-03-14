b0fc78190d1875ce171ea4348c2fab8932d10555
DESKTOP-D467OFD\turpija <turpija@gmail.com>
2023-03-14 11:40:01 +0100

    added readKey, and while loop in main menu

--- ConsoleExercise/Program.cs
+++ ConsoleExercise/Program.cs
@@ -10,18 +10,22 @@ namespace ConsoleExercise
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             void SayHello()
             {
+                StartProgram();
                 Console.Write("Upiši svoje ime: ");
                 string ime = Console.ReadLine();
 
                 Console.WriteLine($"Dobrodošao, {ime}!");
+                EndProgram();
             }
 
             void SayHelloVertically()
             {
+                StartProgram();
                 Console.Write("Upiši svoje ime: ");
                 string ime = Console.ReadLine();
 
@@ -30,10 +34,12 @@ namespace ConsoleExercise
                 {
                     Console.WriteLine($" {ime[i]}");
                 }
+                EndProgram();
             }
 
             void CalculateCircleArea()
             {
+                StartProgram();
                 Console.WriteLine("Izračunaj površinu kružnice");
                 Console.Write("Upiši polumjer: ");
                 string radiusStr = Console.ReadLine();
@@ -42,10 +48,12 @@ namespace ConsoleExercise
                 {
                     Console.Write($"Površina kružnice (čiji je polumjer = {radius}) jest = {Math.PI * Math.Pow(radius, 2)}");
                 }
+                EndProgram();
             }
 
             void SumArray()
             {
+                StartProgram();
                 Console.Write("upiši niz brojeva: ");
                 string input = Console.ReadLine();
                 //string input = "324 4578:6745,23 a bb 2";
@@ -63,10 +71,12 @@ namespace ConsoleExercise
                     }
                 }
                 Console.WriteLine($"Zbroj svih brojeva u nizu = {sum}"); //11672
+                EndProgram();
             }
 
             void BeautifyString()
             {
+                StartProgram();
                 Console.WriteLine("LET's MAKE THIS PRETTY (barem 6 znakova): ");
                 string input = Console.ReadLine();
                 if (input.Length >= 6)
@@ -74,37 +84,60 @@ namespace ConsoleExercise
                     string pocetak = input.Substring(0, 2).ToUpper();
                     string izmedju = input.Substring(2, input.Length - 2).ToLower();
                     string kraj = input.Substring(input.Length - 2, 2).ToUpper();
+                    Console.WriteLine();
                     Console.WriteLine($"..:: {pocetak}{izmedju}{kraj} ::..");
                 }
+                EndProgram();
+            }
+
+            void StartProgram()
+            {
+                Console.Clear();
+            }
+
+            void EndProgram()
+            {
+                Console.WriteLine();
+                Console.Write("...press anykey to return to MainMenu...");
+                Console.ReadKey();
+                StartProgram();
             }
 
             void Selector()
             {
-                Console.WriteLine("the SELECTOR 9000");
-                Console.WriteLine("1 - SayHello");
-                Console.WriteLine("2 - SayHelloVertical");
-                Console.WriteLine("3 - CalculateCircleArea");
-                Console.WriteLine("4 - SumArray");
-                Console.WriteLine("5 - BeautifyString");
-                Console.Write("Type number of choice you with to make:");
-                string input = Console.ReadLine();
-
-                switch (input)
+                Console.Clear();
+                ConsoleKeyInfo keyPress;
+                do
                 {
-                    case "1": SayHello(); break;
-                    case "2": SayHelloVertically(); break;
-                    case "3": CalculateCircleArea(); break;
-                    case "4": SumArray(); break;
-                    case "5": BeautifyString(); break;
-                    default:
-                        Console.WriteLine("error...");
-                        break;
-                }
+                    Console.WriteLine("--== the SELECTOR 9000 ==--");
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine();
+                    Console.WriteLine("(1) SayHello");
+                    Console.WriteLine("(2) SayHelloVertical");
+                    Console.WriteLine("(3) CalculateCircleArea");
+                    Console.WriteLine("(4) SumArray");
+                    Console.WriteLine("(5) BeautifyString");
+                    Console.WriteLine();
+                    Console.Write("Type number of choice you wish to make (Esc to exit):");
+                    keyPress = Console.ReadKey();
+
+                    switch (keyPress.Key.ToString())
+                    {
+                        case "D1": SayHello(); break;
+                        case "D2": SayHelloVertically(); break;
+                        case "D3": CalculateCircleArea(); break;
+                        case "D4": SumArray(); break;
+                        case "D5": BeautifyString(); break;
+                        default:
+                            break;
+                    }
+                } while (keyPress.Key != ConsoleKey.Escape);
             }
 
             Selector();
 
-            Console.ReadLine();
+
+            //Console.ReadLine();
         }
     }
 }
